1dcdfee3af6cb06e5b8aeb02a42cb27cf7e9ca6b
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-07-08 02:00:55 +0200

    Script finished

--- toggle-resolver.sh
+++ toggle-resolver.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-find . -type f -exec sed -i "s/foo/bar/g" {} \;
+find . -type f -name "Cargo.toml" -exec sed -i '
+s/^# resolver = "2"/resolver = "2"/
+t
+s/resolver = "2"/# resolver = "2"/' {} \;
